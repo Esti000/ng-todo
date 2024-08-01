c381f4c4f0c1f35b02475468873c7495abf28933
Esti000 <estimasika1@gmail.com>
2024-08-01 21:28:30 +0300

    Managing task completion and deletion

--- src/app/components/new-todo/new-todo.component.ts
+++ src/app/components/new-todo/new-todo.component.ts
@@ -1,6 +1,10 @@
 import { Component,  ViewChild } from '@angular/core';
-import { Form, NgForm, NgModel, NgModelGroup } from '@angular/forms';
+import {  NgForm, NgModel, NgModelGroup } from '@angular/forms';
 import { MaterialModule } from '../../material/material.module';
+import { IToDo } from '../../models/todo.interface';
+import { TodoService } from '../../services/todo.service';
+import { MatDialog } from '@angular/material/dialog';
+import {v4 as uuidv4} from 'uuid'
 
 @Component({
   selector: 'app-new-todo',
@@ -10,8 +14,27 @@ import { MaterialModule } from '../../material/material.module';
   styleUrl: './new-todo.component.css'
 })
 export class NewTodoComponent {
-@ViewChild("f") form: Form
+@ViewChild("f") form: NgForm
+constructor(public matDialog: MatDialog, private todoservice:TodoService)
+{
+
+}
  public onNewTodoSubmit(): void{
+  // create the new todo object
+  // send object to service
+  const formvalue=this.form.form.value
+  const newTodo: IToDo={
+    id:uuidv4(),
+    title:formvalue.title,
+    description:formvalue.description,
+    endDate:formvalue.date,
+    isArchived:false,
+    isComplited:false,
+    selected:false
+
+  }
+  this.todoservice.addNewToDo(newTodo)
+  this.matDialog.closeAll()
   console.log("On submit")
   console.log(this.form)
 
--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -6,18 +6,18 @@ import { IToDo } from '../models/todo.interface';
   providedIn: 'root'
 })
 export class TodoService {
-private mock: IToDo[]=[
-    {"id":1,"title":"Boa, malagasy ground","description":"Acrantophis madagascariensis","isArchived":false,"endDate":"2/2/2024","isComplited":false, "selected":true},
-{"id":2,"title":"Magistrate black colobus","description":"Colobus guerza","isArchived":false,"endDate":"3/23/2024","isComplited":false,  "selected":false},
-{"id":3,"title":"Giant anteater","description":"Myrmecophaga tridactyla","isArchived":false,"endDate":"10/20/2023","isComplited":false,"selected":false},
-{"id":4,"title":"Cobra, cape","description":"Naja nivea","isArchived":false,"endDate":"1/1/2024","isComplited":false, "selected":false},
-{"id":5,"title":"Flicker, campo","description":"Colaptes campestroides","isArchived":false,"endDate":"5/14/2024","isComplited":false, "selected":false},
-  ] 
-
-  private _TodoSubject: BehaviorSubject<Array<IToDo>>=new BehaviorSubject(this.mock)
+// private mock: IToDo[]=[
+//     {"id":1,"title":"Boa, malagasy ground","description":"Acrantophis madagascariensis","isArchived":false,"endDate":"2/2/2024","isComplited":false, "selected":true},
+// {"id":2,"title":"Magistrate black colobus","description":"Colobus guerza","isArchived":false,"endDate":"3/23/2024","isComplited":false,  "selected":false},
+// {"id":3,"title":"Giant anteater","description":"Myrmecophaga tridactyla","isArchived":false,"endDate":"10/20/2023","isComplited":false,"selected":false},
+// {"id":4,"title":"Cobra, cape","description":"Naja nivea","isArchived":false,"endDate":"1/1/2024","isComplited":false, "selected":false},
+// {"id":5,"title":"Flicker, campo","description":"Colaptes campestroides","isArchived":false,"endDate":"5/14/2024","isComplited":false, "selected":false},
+  // ] 
+  private ToDos:IToDo[] | Array<IToDo>=[]
+  private _TodoSubject: BehaviorSubject<Array<IToDo>>=new BehaviorSubject(this.ToDos)
   constructor() { }
 
-  private _singleToDoSubject:BehaviorSubject<IToDo>=new BehaviorSubject(this.mock[0])
+  private _singleToDoSubject:BehaviorSubject<IToDo>=new BehaviorSubject(this.ToDos.length? this.ToDos[0]:null)
 
   public getSelectedTodo(): Observable<IToDo>
   {
@@ -32,5 +32,13 @@ private mock: IToDo[]=[
   public setSelectedTodo(todo:IToDo)
   {
     this._singleToDoSubject.next(todo)
+  }
+  public addNewToDo(newTodo:IToDo): void
+  {
+    console.log(newTodo.id)
+    // אני מחזיקה את מערך האובסרבבל כי רק כך הקומפוננטוט יוכלו לתקשר איתו ואליו אני מוסיפה
+    const ExitlistOfToDo: Array<IToDo> =this._TodoSubject.value;
+    ExitlistOfToDo.push(newTodo)
+    this._TodoSubject.next(ExitlistOfToDo)
   }
 }
